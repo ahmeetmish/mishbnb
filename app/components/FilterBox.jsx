e47a420cfe32144315bb571ac2574cab5d1e2ace
ahmet <codingwithmish@gmail.com>
2024-02-11 17:46:01 +0300

    fixed filterbox showing on another page

--- app/components/FilterBox.jsx
+++ app/components/FilterBox.jsx
@@ -1,11 +1,20 @@
 'use client'
 
+import { usePathname } from "next/navigation"
+
 import useFilterModal from "@/app/hooks/useFilterModal"
 
 import { MdFilterList, MdFilterListOff } from "react-icons/md"
 
 export default function FilterBox() {
+  const pathname = usePathname()
   const filterModal = useFilterModal()
+
+  const isMainPage = pathname == '/'
+
+  if(!isMainPage) {
+    return null
+  }
 
   return (
     <div onClick={filterModal.onOpen} className="gap-1 flex items-center p-3 border-[1px] rounded-xl shadow-sm cursor-pointer">
